6666aba0546cc69b8b0c8925d10951b75dfc8e9f
Albert <wuyichang1990@gmail.com>
2019-10-01 11:27:55 +0100

    Update scripy.py

--- scripy.py
+++ scripy.py
@@ -4,6 +4,6 @@ from subprocess import Popen
 import time
 for i in range(4):
     command = 'python main.py 889{} id{}'.format(i+1,i+1)
-    # can't create peers two fast. 
+    # can't create peers too fast. 
     time.sleep(0.2) 
     Popen(['xterm','-e',command])
